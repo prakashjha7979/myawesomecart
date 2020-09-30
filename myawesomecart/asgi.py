f6d47c27821b0c75f22b53fb429400e6843dbecb
prakash jha <prakashjhakk.pj@gmail.com>
2020-09-30 14:01:41 +0530

    Initial commit

---
+++ myawesomecart/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for myawesomecart project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myawesomecart.settings')
+
+application = get_asgi_application()
